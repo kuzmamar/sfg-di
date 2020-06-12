dc5f3a79b9ba76e6d5bc611f22378df58f11110b
Martin Kuzma <martin.kuzma90@gmail.com>
2020-06-12 15:48:24 +0200

    Initialize Spring project using Initializr

---
+++ src/main/java/kuzmic/sfgdi/SfgDiApplication.java
@@ -0,0 +1,13 @@
+package kuzmic.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiApplication.class, args);
+	}
+
+}
